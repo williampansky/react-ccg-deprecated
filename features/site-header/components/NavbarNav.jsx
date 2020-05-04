c5778f75177ce7fc592493733eec9a8cbc8d4e0d
William Pansky <williampansky@gmail.com>
2020-05-04 15:08:55 -0500

    added active state

--- features/site-header/components/NavbarNav.jsx
+++ features/site-header/components/NavbarNav.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from './navbar-nav.module.scss';
 
 const NavbarNav = ({ data }) => {
+  const router = useRouter();
+  const { pathname } = router;
+
   return (
     <ul className={styles.nav}>
       {data.map((obj, idx) => {
         const { name, path } = obj;
         return (
-          <li key={idx}>
+          <li className={pathname === path ? styles.active : ''} key={idx}>
             <Link href={path}>
               <a className={styles.item}>{name}</a>
             </Link>
