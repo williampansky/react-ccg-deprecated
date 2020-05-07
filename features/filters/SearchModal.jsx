d9bb35fd14783933d4f04d08ee89a9b80d85dbbd
William Pansky <williampansky@gmail.com>
2020-05-06 22:43:53 -0500

    updates

--- features/filters/SearchModal.jsx
+++ features/filters/SearchModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { ReactSVG } from 'react-svg';
 import { toggleSearchModal, setSearchValue } from './filters.slice';
@@ -7,11 +7,19 @@ export default function SearchModal() {
   const dispatch = useDispatch();
   const { searchModalActive } = useSelector(s => s.filters);
   const [value, setValue] = useState(null);
+  const ref = useRef();
 
   function handleSubmit(event, formValue = value) {
     event.preventDefault();
     dispatch(toggleSearchModal());
     return dispatch(setSearchValue(formValue ? formValue : null));
+  }
+
+  function clearValue(event) {
+    const { current } = ref;
+    event.target.blur();
+    current.value = '';
+    return setValue(null);
   }
 
   return (
@@ -34,8 +42,18 @@ export default function SearchModal() {
           onSubmit={event => handleSubmit(event, value)}
         >
           <div>
+            <div
+              className="search__clear__all"
+              onClick={event => clearValue(event)}
+              onKeyPress={event => clearValue(event)}
+              role="button"
+              tabIndex={0}
+            >
+              <ReactSVG src="/images/site/logo-uikit-close.svg" />
+            </div>
             <input
               onChange={event => setValue(event.target.value)}
+              ref={ref}
               type="text"
             />
           </div>
