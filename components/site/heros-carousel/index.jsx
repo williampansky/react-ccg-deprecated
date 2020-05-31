5287105e2c9258447a4a969d12fdafb8fe594e03
William Pansky <williampansky@gmail.com>
2020-05-31 15:24:31 -0500

    working on hero caro

--- components/site/heros-carousel/index.jsx
+++ components/site/heros-carousel/index.jsx
@@ -22,6 +22,7 @@ const HerosCarousel = () => {
   const [randomHeros, setRandomHeros] = useState([]);
   const [currentHero, setCurrentHero] = useState(null);
   const { isDesktop } = useResponsive();
+  const symbol = currentHero && currentHero.symbol;
 
   const randomHerosCB = useCallback((array, size) => {
     let shuffled = array.slice(0),
@@ -53,43 +54,47 @@ const HerosCarousel = () => {
     subheadline: `Who will you take to victory?`
   };
 
-  function buttonHandler(event) {
-    const { target } = event;
-    const attr = target.getAttribute('aria-label');
-    target.blur();
+  const buttonHandler = useCallback(
+    event => {
+      const { target } = event;
+      const attr = target.getAttribute('aria-label');
+      target.blur();
 
-    if (attr === 'previous') {
-      if (currentHero.symbol === randomHeros[2].symbol) {
-        setCurrentSlide(1);
-        return setCurrentHero(randomHeros[1]);
+      if (attr === 'previous') {
+        if (symbol === randomHeros[2].symbol) {
+          setCurrentSlide(1);
+          return setCurrentHero(randomHeros[1]);
+        }
+
+        if (symbol === randomHeros[1].symbol) {
+          setCurrentSlide(0);
+          return setCurrentHero(randomHeros[0]);
+        }
       }
 
-      if (currentHero.symbol === randomHeros[1].symbol) {
-        setCurrentSlide(0);
-        return setCurrentHero(randomHeros[0]);
-      }
-    }
+      if (attr === 'next') {
+        if (symbol === randomHeros[0].symbol) {
+          setCurrentSlide(1);
+          return setCurrentHero(randomHeros[1]);
+        }
 
-    if (attr === 'next') {
-      if (currentHero.symbol === randomHeros[0].symbol) {
-        setCurrentSlide(1);
-        return setCurrentHero(randomHeros[1]);
+        if (symbol === randomHeros[1].symbol) {
+          setCurrentSlide(2);
+          return setCurrentHero(randomHeros[2]);
+        }
       }
-
-      if (currentHero.symbol === randomHeros[1].symbol) {
-        setCurrentSlide(2);
-        return setCurrentHero(randomHeros[2]);
-      }
-    }
-  }
+    },
+    [symbol, randomHeros]
+  );
 
   function dotHandler(event) {
+    if (!event) return;
     const { target } = event;
-    // const index = target.getAttribute('data-index');
-    // target.blur();
-    // console.log(index);
-    // setCurrentSlide(index);
-    // return setCurrentHero(randomHeros[index]);
+    if (!target) return;
+    const index = target.getAttribute('data-index');
+    target.blur();
+    setCurrentSlide(index);
+    return setCurrentHero(randomHeros[index]);
   }
 
   return (
@@ -110,122 +115,125 @@ const HerosCarousel = () => {
           touchEnabled={false}
           visibleSlides={1}
         >
-          <ButtonBack onClick={buttonHandler}>
-            <div className="btn-outer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                viewBox="0 0 200 200"
-              >
-                <defs>
+          <div className="button__wrapper">
+            <ButtonBack onClick={buttonHandler}>
+              <div className="btn-outer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 200 200"
+                >
+                  <defs>
+                    <linearGradient
+                      id="linear-gradient"
+                      x1="100"
+                      y1="6"
+                      x2="100"
+                      y2="194"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop offset="0" stopColor="#b47555"></stop>
+                      <stop offset="1" stopColor="#68462d"></stop>
+                    </linearGradient>
+                    <linearGradient
+                      id="linear-gradient-2"
+                      x1="100"
+                      y1="24"
+                      x2="100"
+                      y2="176"
+                      xlinkHref="#linear-gradient"
+                    ></linearGradient>
+                  </defs>
+                  <path
+                    d="M100 194a94 94 0 1 1 94-94 94.11 94.11 0 0 1-94 94zm0-186a92 92 0 1 0 92 92 92.1 92.1 0 0 0-92-92z"
+                    fill="url(#linear-gradient)"
+                  ></path>
+                  <path
+                    d="M100 176a76 76 0 1 1 76-76 76.08 76.08 0 0 1-76 76zm0-148a72 72 0 1 0 72 72 72.08 72.08 0 0 0-72-72z"
+                    fill="url(#linear-gradient-2)"
+                  ></path>
+                </svg>
+              </div>
+              <div className="btn-inner">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28">
                   <linearGradient
-                    id="linear-gradient"
-                    x1="100"
-                    y1="6"
-                    x2="100"
-                    y2="194"
+                    id="arrow-button-right-a"
                     gradientUnits="userSpaceOnUse"
+                    x1="14"
+                    y1="5.503"
+                    x2="14"
+                    y2="29.902"
                   >
                     <stop offset="0" stopColor="#b47555"></stop>
                     <stop offset="1" stopColor="#68462d"></stop>
                   </linearGradient>
-                  <linearGradient
-                    id="linear-gradient-2"
-                    x1="100"
-                    y1="24"
-                    x2="100"
-                    y2="176"
-                    xlinkHref="#linear-gradient"
-                  ></linearGradient>
-                </defs>
-                <path
-                  d="M100 194a94 94 0 1 1 94-94 94.11 94.11 0 0 1-94 94zm0-186a92 92 0 1 0 92 92 92.1 92.1 0 0 0-92-92z"
-                  fill="url(#linear-gradient)"
-                ></path>
-                <path
-                  d="M100 176a76 76 0 1 1 76-76 76.08 76.08 0 0 1-76 76zm0-148a72 72 0 1 0 72 72 72.08 72.08 0 0 0-72-72z"
-                  fill="url(#linear-gradient-2)"
-                ></path>
-              </svg>
-            </div>
-            <div className="btn-inner">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28">
-                <linearGradient
-                  id="arrow-button-right-a"
-                  gradientUnits="userSpaceOnUse"
-                  x1="14"
-                  y1="5.503"
-                  x2="14"
-                  y2="29.902"
+                  <path
+                    fill="url(#arrow-button-right-a)"
+                    d="M13.5 1.1l-4 3.4L16 11H1.8v5.2H16l-7 7 3.8 3.7 13.4-13.4z"
+                  ></path>
+                </svg>
+              </div>
+            </ButtonBack>
+            <ButtonNext onClick={buttonHandler}>
+              <div className="btn-outer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 200 200"
                 >
-                  <stop offset="0" stopColor="#b47555"></stop>
-                  <stop offset="1" stopColor="#68462d"></stop>
-                </linearGradient>
-                <path
-                  fill="url(#arrow-button-right-a)"
-                  d="M13.5 1.1l-4 3.4L16 11H1.8v5.2H16l-7 7 3.8 3.7 13.4-13.4z"
-                ></path>
-              </svg>
-            </div>
-          </ButtonBack>
-          <ButtonNext onClick={buttonHandler}>
-            <div className="btn-outer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                viewBox="0 0 200 200"
-              >
-                <defs>
+                  <defs>
+                    <linearGradient
+                      id="linear-gradient"
+                      x1="100"
+                      y1="6"
+                      x2="100"
+                      y2="194"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop offset="0" stopColor="#b47555"></stop>
+                      <stop offset="1" stopColor="#68462d"></stop>
+                    </linearGradient>
+                    <linearGradient
+                      id="linear-gradient-2"
+                      x1="100"
+                      y1="24"
+                      x2="100"
+                      y2="176"
+                      xlinkHref="#linear-gradient"
+                    ></linearGradient>
+                  </defs>
+                  <path
+                    d="M100 194a94 94 0 1 1 94-94 94.11 94.11 0 0 1-94 94zm0-186a92 92 0 1 0 92 92 92.1 92.1 0 0 0-92-92z"
+                    fill="url(#linear-gradient)"
+                  ></path>
+                  <path
+                    d="M100 176a76 76 0 1 1 76-76 76.08 76.08 0 0 1-76 76zm0-148a72 72 0 1 0 72 72 72.08 72.08 0 0 0-72-72z"
+                    fill="url(#linear-gradient-2)"
+                  ></path>
+                </svg>
+              </div>
+              <div className="btn-inner">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28">
                   <linearGradient
-                    id="linear-gradient"
-                    x1="100"
-                    y1="6"
-                    x2="100"
-                    y2="194"
+                    id="arrow-button-right-a"
                     gradientUnits="userSpaceOnUse"
+                    x1="14"
+                    y1="5.503"
+                    x2="14"
+                    y2="29.902"
                   >
                     <stop offset="0" stopColor="#b47555"></stop>
                     <stop offset="1" stopColor="#68462d"></stop>
                   </linearGradient>
-                  <linearGradient
-                    id="linear-gradient-2"
-                    x1="100"
-                    y1="24"
-                    x2="100"
-                    y2="176"
-                    xlinkHref="#linear-gradient"
-                  ></linearGradient>
-                </defs>
-                <path
-                  d="M100 194a94 94 0 1 1 94-94 94.11 94.11 0 0 1-94 94zm0-186a92 92 0 1 0 92 92 92.1 92.1 0 0 0-92-92z"
-                  fill="url(#linear-gradient)"
-                ></path>
-                <path
-                  d="M100 176a76 76 0 1 1 76-76 76.08 76.08 0 0 1-76 76zm0-148a72 72 0 1 0 72 72 72.08 72.08 0 0 0-72-72z"
-                  fill="url(#linear-gradient-2)"
-                ></path>
-              </svg>
-            </div>
-            <div className="btn-inner">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28">
-                <linearGradient
-                  id="arrow-button-right-a"
-                  gradientUnits="userSpaceOnUse"
-                  x1="14"
-                  y1="5.503"
-                  x2="14"
-                  y2="29.902"
-                >
-                  <stop offset="0" stopColor="#b47555"></stop>
-                  <stop offset="1" stopColor="#68462d"></stop>
-                </linearGradient>
-                <path
-                  fill="url(#arrow-button-right-a)"
-                  d="M13.5 1.1l-4 3.4L16 11H1.8v5.2H16l-7 7 3.8 3.7 13.4-13.4z"
-                ></path>
-              </svg>
-            </div>
-          </ButtonNext>
+                  <path
+                    fill="url(#arrow-button-right-a)"
+                    d="M13.5 1.1l-4 3.4L16 11H1.8v5.2H16l-7 7 3.8 3.7 13.4-13.4z"
+                  ></path>
+                </svg>
+              </div>
+            </ButtonNext>
+          </div>
+
           <Slider trayProps={{ onChange: buttonHandler }}>
             {randomHeros.length ? (
               <React.Fragment>
@@ -241,11 +249,11 @@ const HerosCarousel = () => {
               </React.Fragment>
             ) : null}
           </Slider>
-          <DotGroup renderDots={dotHandler}>
+          {/* <DotGroup renderDots={dotHandler}>
             <Dot data-index={0} slide={0} onClick={dotHandler} />
             <Dot data-index={1} slide={1} onClick={dotHandler} />
             <Dot data-index={2} slide={2} onClick={dotHandler} />
-          </DotGroup>
+          </DotGroup> */}
         </CarouselProvider>
       </div>
       <div className={styles['content']}>
