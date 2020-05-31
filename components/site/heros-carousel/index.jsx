e584ff1ac267c90607c8f731bb325033c0087e45
William Pansky <williampansky@gmail.com>
2020-05-31 16:51:05 -0500

    carousel tweaks

--- components/site/heros-carousel/index.jsx
+++ components/site/heros-carousel/index.jsx
@@ -58,27 +58,36 @@ const HerosCarousel = () => {
     event => {
       const { target } = event;
       const attr = target.getAttribute('aria-label');
+      if (!target || !exists(attr)) return;
       target.blur();
 
       if (attr === 'previous') {
-        if (symbol === randomHeros[2].symbol) {
+        if (
+          symbol === (randomHeros && randomHeros[2] && randomHeros[2].symbol)
+        ) {
           setCurrentSlide(1);
           return setCurrentHero(randomHeros[1]);
         }
 
-        if (symbol === randomHeros[1].symbol) {
+        if (
+          symbol === (randomHeros && randomHeros[1] && randomHeros[1].symbol)
+        ) {
           setCurrentSlide(0);
           return setCurrentHero(randomHeros[0]);
         }
       }
 
       if (attr === 'next') {
-        if (symbol === randomHeros[0].symbol) {
+        if (
+          symbol === (randomHeros && randomHeros[0] && randomHeros[0].symbol)
+        ) {
           setCurrentSlide(1);
           return setCurrentHero(randomHeros[1]);
         }
 
-        if (symbol === randomHeros[1].symbol) {
+        if (
+          symbol === (randomHeros && randomHeros[1] && randomHeros[1].symbol)
+        ) {
           setCurrentSlide(2);
           return setCurrentHero(randomHeros[2]);
         }
@@ -123,54 +132,13 @@ const HerosCarousel = () => {
                   xmlnsXlink="http://www.w3.org/1999/xlink"
                   viewBox="0 0 200 200"
                 >
-                  <defs>
-                    <linearGradient
-                      id="linear-gradient"
-                      x1="100"
-                      y1="6"
-                      x2="100"
-                      y2="194"
-                      gradientUnits="userSpaceOnUse"
-                    >
-                      <stop offset="0" stopColor="#b47555"></stop>
-                      <stop offset="1" stopColor="#68462d"></stop>
-                    </linearGradient>
-                    <linearGradient
-                      id="linear-gradient-2"
-                      x1="100"
-                      y1="24"
-                      x2="100"
-                      y2="176"
-                      xlinkHref="#linear-gradient"
-                    ></linearGradient>
-                  </defs>
-                  <path
-                    d="M100 194a94 94 0 1 1 94-94 94.11 94.11 0 0 1-94 94zm0-186a92 92 0 1 0 92 92 92.1 92.1 0 0 0-92-92z"
-                    fill="url(#linear-gradient)"
-                  ></path>
-                  <path
-                    d="M100 176a76 76 0 1 1 76-76 76.08 76.08 0 0 1-76 76zm0-148a72 72 0 1 0 72 72 72.08 72.08 0 0 0-72-72z"
-                    fill="url(#linear-gradient-2)"
-                  ></path>
+                  <path d="M100 194a94 94 0 1 1 94-94 94.11 94.11 0 0 1-94 94zm0-186a92 92 0 1 0 92 92 92.1 92.1 0 0 0-92-92z" />
+                  <path d="M100 176a76 76 0 1 1 76-76 76.08 76.08 0 0 1-76 76zm0-148a72 72 0 1 0 72 72 72.08 72.08 0 0 0-72-72z" />
                 </svg>
               </div>
               <div className="btn-inner">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28">
-                  <linearGradient
-                    id="arrow-button-right-a"
-                    gradientUnits="userSpaceOnUse"
-                    x1="14"
-                    y1="5.503"
-                    x2="14"
-                    y2="29.902"
-                  >
-                    <stop offset="0" stopColor="#b47555"></stop>
-                    <stop offset="1" stopColor="#68462d"></stop>
-                  </linearGradient>
-                  <path
-                    fill="url(#arrow-button-right-a)"
-                    d="M13.5 1.1l-4 3.4L16 11H1.8v5.2H16l-7 7 3.8 3.7 13.4-13.4z"
-                  ></path>
+                  <path d="M13.5 1.1l-4 3.4L16 11H1.8v5.2H16l-7 7 3.8 3.7 13.4-13.4z" />
                 </svg>
               </div>
             </ButtonBack>
@@ -181,59 +149,17 @@ const HerosCarousel = () => {
                   xmlnsXlink="http://www.w3.org/1999/xlink"
                   viewBox="0 0 200 200"
                 >
-                  <defs>
-                    <linearGradient
-                      id="linear-gradient"
-                      x1="100"
-                      y1="6"
-                      x2="100"
-                      y2="194"
-                      gradientUnits="userSpaceOnUse"
-                    >
-                      <stop offset="0" stopColor="#b47555"></stop>
-                      <stop offset="1" stopColor="#68462d"></stop>
-                    </linearGradient>
-                    <linearGradient
-                      id="linear-gradient-2"
-                      x1="100"
-                      y1="24"
-                      x2="100"
-                      y2="176"
-                      xlinkHref="#linear-gradient"
-                    ></linearGradient>
-                  </defs>
-                  <path
-                    d="M100 194a94 94 0 1 1 94-94 94.11 94.11 0 0 1-94 94zm0-186a92 92 0 1 0 92 92 92.1 92.1 0 0 0-92-92z"
-                    fill="url(#linear-gradient)"
-                  ></path>
-                  <path
-                    d="M100 176a76 76 0 1 1 76-76 76.08 76.08 0 0 1-76 76zm0-148a72 72 0 1 0 72 72 72.08 72.08 0 0 0-72-72z"
-                    fill="url(#linear-gradient-2)"
-                  ></path>
+                  <path d="M100 194a94 94 0 1 1 94-94 94.11 94.11 0 0 1-94 94zm0-186a92 92 0 1 0 92 92 92.1 92.1 0 0 0-92-92z" />
+                  <path d="M100 176a76 76 0 1 1 76-76 76.08 76.08 0 0 1-76 76zm0-148a72 72 0 1 0 72 72 72.08 72.08 0 0 0-72-72z" />
                 </svg>
               </div>
               <div className="btn-inner">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28">
-                  <linearGradient
-                    id="arrow-button-right-a"
-                    gradientUnits="userSpaceOnUse"
-                    x1="14"
-                    y1="5.503"
-                    x2="14"
-                    y2="29.902"
-                  >
-                    <stop offset="0" stopColor="#b47555"></stop>
-                    <stop offset="1" stopColor="#68462d"></stop>
-                  </linearGradient>
-                  <path
-                    fill="url(#arrow-button-right-a)"
-                    d="M13.5 1.1l-4 3.4L16 11H1.8v5.2H16l-7 7 3.8 3.7 13.4-13.4z"
-                  ></path>
+                  <path d="M13.5 1.1l-4 3.4L16 11H1.8v5.2H16l-7 7 3.8 3.7 13.4-13.4z" />
                 </svg>
               </div>
             </ButtonNext>
           </div>
-
           <Slider trayProps={{ onChange: buttonHandler }}>
             {randomHeros.length ? (
               <React.Fragment>
